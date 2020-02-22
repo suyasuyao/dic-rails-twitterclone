faf21ea591b33dc4ea6446c46d7de48b166e366f
yasuyasu-ot <y.otsubo.dss@gmail.com>
2020-02-22 20:56:09 +0900

    mentionテーブルを実現するマイグレーションファイルの作成 #5

--- hello_caht/db/migrate/20200222092441_create_mentions.rb
+++ hello_caht/db/migrate/20200222092441_create_mentions.rb
@@ -1,7 +1,8 @@
 class CreateMentions < ActiveRecord::Migration[5.2]
   def change
     create_table :mentions do |t|
-
+      # mentionsテーブルはcontentカラムのみ追加(not null制約あり)
+      t.string :content, null: false
       t.timestamps
     end
   end
